545d8ce7b6662506761038ac190b0405f9b2584b
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-03-21 12:44:30 +0100

    add deleting students by index number

---
+++ Utils/StudentValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using cwiczenia2_zen_s19743.Model;
+
+namespace cwiczenia2_zen_s19743.Utils
+{
+    public static class StudentValidator
+    {
+        public static bool CheckIfStudentExist(string indexNumber, List<Student> studentList)
+        {
+            return studentList.Any(student => student.IndexNumber.Equals(indexNumber));
+        }
+
+        public static bool CheckIfStudentHasNullFields(Student student)
+        {
+            return student.GetType().GetProperties().Any(info => info.GetValue(student) == null);
+        }
+    }
+}
